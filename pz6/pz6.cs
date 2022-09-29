d31cb4c53e57d95403f9f031429dfa24eb06b146
Kurlaev <vinisihko01@yandex.ru>
2022-09-29 10:37:13 +0500

    pz6

---
+++ pz6/pz6.cs
@@ -0,0 +1,19 @@
+﻿namespace pz6
+{
+    internal class pz6
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число n: ");
+            double result = 0;
+            double n = Convert.ToDouble(Console.ReadLine());
+            double i = 1;
+            while (i <= n)
+            {
+                result += 1 / i;
+                i++;
+            }
+            Console.WriteLine($"Результат равен: {result}");
+        }
+    }
+}
