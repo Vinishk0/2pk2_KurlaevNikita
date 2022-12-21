086dd33cccc2e12cab6ed0786358c4406163b268
Алексей Арапов <aleksejsoroh@gmail.com>
2022-12-21 09:00:45 +0500

    pz16

---
+++ pz16/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+namespace pz16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file1 = @""; 
+            FileInfo file = new FileInfo(file1);
+            if (!file.Exists)
+            {
+                file.Create();
+            }
+
+            Console.WriteLine("Введите текст: ");
+
+            using (StreamWriter sw = new StreamWriter(file1))
+            {
+                string line = "";
+                line = Console.ReadLine();
+                sw.WriteLine(line);
+                string digits = Regex.Replace(line, @"\D", "");
+                Console.WriteLine("Количество цифр: " + digits.Length);
+                sw.Close();
+                Console.ReadLine();
+            }
+            using (StreamReader sr = new StreamReader(file1))
+            {
+                Console.WriteLine(sr.ReadToEnd());
+            }
+        }
+    }
+}
